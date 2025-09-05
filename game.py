4d9abd6a29ec160eb50c5f1a7330816ea37ee7b3
Louis Tran <louis@steamforvietnam.org>
2025-09-05 20:49:46 +0800

    milestone5: remove item on touch

--- game.py
+++ game.py
@@ -39,10 +39,30 @@ for x,y in data["items"]:
     item = canvas.create_oval(x, y, x+5, y+5, fill='white')
     items.append(item)
 
+def touch():
+    # lấy vị trí của player
+    x1, y1, x2, y2 = canvas.coords(player)
+
+    # tìm tất cả các item ở trong vùng của player
+    found_items = canvas.find_overlapping(x1, y1, x2, y2)
+
+    for item in found_items:
+        # Nếu item đó nằm trong danh sách items
+        if item in items:
+           canvas.delete(item)
+           items.remove(item)
+
+    # nếu không còn item nào, trò chơi kết thúc
+    if len(items) == 0:
+       print("END!")
+
 def update_score():
     # thay đổi số điểm
     data["score"] = data["score"] + 1
     score_label.configure(text="Số bước: "+str(data["score"]))
+
+    # kiểm tra xem có chạm item nào không
+    touch()
 
 def up_handler(event):
     canvas.move(player, 0, -10)
